8e47fa67830ecc010455c29d3845cd8e6aaeda7b
ApanLoon <apanloon@live.co.uk>
2022-03-18 07:03:57 +0100

    Added some tools to get example results from openupm.

---
+++ tools/openupm-get.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl https://package.openupm.com/com.gemserk.upmgitpusher --output openupm-get.json
---
+++ tools/openupm-search.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl https://package.openupm.com/-/v1/search?size=3 --output openupm-search.json
